4bd70dbbef44ba08d89812d77c6631e3f0784f65
Marius Bekk <marius.bekk@intility.no>
2023-02-03 15:36:34 +0100

    Vercel issues

--- components/SkillBar.jsx
+++ components/SkillBar.jsx
@@ -1,18 +1,18 @@
 "use client";
 
 const SkillBar = ({ title, barColor, barPercentage }) => (
-  <>
+  <div>
     <h1 className="sm:pt-5 text-white pt-5 ">
       {title} <span className=" text-xs text-yellow-400">{barPercentage}%</span>
     </h1>
     <div className="mt-2 h-4 relative w-60 rounded-full overflow-hidden">
       <div className=" w-full h-full bg-gray-200 absolute "></div>
       <div
-        classNameName={`h-full ${barColor} sm:${barColor} absolute`}
+        className={`h-full ${barColor} sm:${barColor} absolute`}
         style={{ width: `${barPercentage}%` }}
       ></div>
     </div>
-  </>
+  </div>
 );
 
 export default SkillBar;
